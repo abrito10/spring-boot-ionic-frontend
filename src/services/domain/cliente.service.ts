8635dd286e33e6a79cf461f58553727bac38cf32
Adilson Brito <abrito10@gmail.com>
2019-12-17 16:23:21 -0300

    147. Tela de confirmação de pedido

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -14,13 +14,20 @@ export class ClienteService {
         public storage : StorageService){            
         }
      
-    findByEmail(email : string) {
+        findByEmail(email : string) {
 
-        return this.http.get(
-            `${API_CONFIG.baseUrl}/clientes/email?value=${email}` 
-            );
+            return this.http.get(
+                `${API_CONFIG.baseUrl}/clientes/email?value=${email}` 
+                );
         }
 
+        findById(id : string) {
+
+            return this.http.get(
+                `${API_CONFIG.baseUrl}/clientes/${id}` 
+                );
+        }
+            
     getImageFromBucket(id : string) : Observable<any>{
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
         return this.http.get(url,{responseType : 'blob'});
